a4bff68bf39aa597cf4cd8109ded0e72e1e6cd6e
richardimaoka <richard.s.imaoka@gmail.com>
2026-02-15 04:41:42 +0900

    colored panels

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 // import Image from "next/image";
 
 export default function Home() {
-  return <div className="h-full flex items-center justify-center">hello</div>;
+  return (
+    <main>
+      <div className="h-screen bg-[#81A1C1]"></div>
+      <div className="h-screen bg-[#D08770]"></div>
+      <div className="h-screen bg-[#A3BE8C]"></div>
+      <div className="h-screen bg-[#EBCB8B]"></div>
+    </main>
+  );
 }
