3edf23cd61c9397160f342c7985e7964bbaf7481
richardimaoka <richard.s.imaoka@gmail.com>
2026-02-15 04:44:09 +0900

    hide overflows by overflow-y: auto

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main>
+    <main className="h-screen overflow-y-auto">
       <div className="h-screen bg-[#81A1C1]"></div>
       <div className="h-screen bg-[#D08770]"></div>
       <div className="h-screen bg-[#A3BE8C]"></div>
