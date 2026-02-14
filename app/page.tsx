814b48a377324589dc2afa2d48181ba79a404ab1
richardimaoka <richard.s.imaoka@gmail.com>
2026-02-15 05:03:30 +0900

    text color black

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@
 export default function Home() {
   return (
     <main className="h-screen overflow-y-auto snap-y snap-mandatory">
-      <div className="h-screen bg-[#81A1C1] snap-start flex items-center justify-center text-9xl">1</div>
-      <div className="h-screen bg-[#D08770] snap-start flex items-center justify-center text-9xl">2</div>
-      <div className="h-screen bg-[#A3BE8C] snap-start flex items-center justify-center text-9xl">3</div>
-      <div className="h-screen bg-[#EBCB8B] snap-start flex items-center justify-center text-9xl">4</div>
+      <div className="h-screen bg-[#81A1C1] snap-start flex items-center justify-center text-9xl text-black">1</div>
+      <div className="h-screen bg-[#D08770] snap-start flex items-center justify-center text-9xl text-black">2</div>
+      <div className="h-screen bg-[#A3BE8C] snap-start flex items-center justify-center text-9xl text-black">3</div>
+      <div className="h-screen bg-[#EBCB8B] snap-start flex items-center justify-center text-9xl text-black">4</div>
     </main>
   );
 }
