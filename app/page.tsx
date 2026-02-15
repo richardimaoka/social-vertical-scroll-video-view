56e5af8c096c49afed346234da14182823a8686b
richardimaoka <richard.s.imaoka@gmail.com>
2026-02-15 12:56:19 +0900

    limit width

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,55 @@
 
 export default function Home() {
   return (
-    <main className="h-screen overflow-y-auto snap-y snap-mandatory">
-      <div className="h-screen bg-[#81A1C1] snap-start flex items-center justify-center text-9xl text-black">1</div>
-      <div className="h-screen bg-[#D08770] snap-start flex items-center justify-center text-9xl text-black">2</div>
-      <div className="h-screen bg-[#A3BE8C] snap-start flex items-center justify-center text-9xl text-black">3</div>
-      <div className="h-screen bg-[#EBCB8B] snap-start flex items-center justify-center text-9xl text-black">4</div>
+    <main className="h-screen overflow-y-auto snap-y snap-mandatory max-w-[56.25vh] mx-auto">
+      <div className="h-screen bg-[#81A1C1] snap-start flex items-center justify-center">
+        <video
+          width="405"
+          height="720"
+          src="/videos/1_屋外でアイスクリームを食べている女性_720x405.mp4"
+          loop
+          muted
+          autoPlay
+          playsInline
+          className="w-full h-full object-cover"
+        ></video>
+      </div>
+      <div className="h-screen bg-[#D08770] snap-start flex items-center justify-center">
+        <video
+          width="405"
+          height="720"
+          src="/videos/2_ハニーディッパーに垂れるハチミツ_720x405.mp4"
+          loop
+          muted
+          autoPlay
+          playsInline
+          className="w-full h-full object-cover"
+        ></video>
+      </div>
+      <div className="h-screen bg-[#A3BE8C] snap-start flex items-center justify-center">
+        <video
+          width="405"
+          height="720"
+          src="/videos/3_玉ねぎと新玉ねぎ_720x405.mp4"
+          loop
+          muted
+          autoPlay
+          playsInline
+          className="w-full h-full object-cover"
+        ></video>
+      </div>
+      <div className="h-screen bg-[#EBCB8B] snap-start flex items-center justify-center">
+        <video
+          width="405"
+          height="720"
+          src="/videos/4_鏡を見ながらくしを通す女の子_720x405.mp4"
+          loop
+          muted
+          autoPlay
+          playsInline
+          className="w-full h-full object-cover"
+        ></video>
+      </div>
     </main>
   );
 }
